fe920f4fb3593df258ddbf6af50dbba87787ef70
BLACKID25 <25ingchourio@gmail.com>
2025-02-24 23:34:49 -0300

    Actualizado 24-02-25

--- app/components/webHome/Info/info.jsx
+++ app/components/webHome/Info/info.jsx
@@ -2,49 +2,11 @@
 
 import React from "react";
 import { Container} from "semantic-ui-react";
-
+import { itemsData } from "@/app/Hooks/Banner.info";
 import { map } from "lodash";
 import "./info.css";
 
 export function Info() {
-
-    const itemsData = [
-        {
-         
-          title: "URL Personalizada",
-          description:
-            "Obtienes una URL Profesional y más personalizada sin guiones ni puntos y caracteres especiales.",
-        },
-        {
-          
-          title: "Acceso 24/7",
-          description:
-            "Disponible y Accesible 24/7 segun tu plan de preferencia sin importar día y hora.",
-        },
-        {
-          
-          title: "Planes Economicos y Accesibles",
-          description:
-            "Planes accesibles a tu bolsillo y al alcance de tu mano.",
-        },
-        {
-         
-          title: "Mejora tu perfil",
-          description:
-            "Plan Full incluye asesorias con mentores expertos en CV, Perfiles de RRSS y mucho más.",
-        },
-        {
-          
-          title: "Marketing a Empresas",
-          description:
-            "Con tu afiliación, un pequeño % se va al marketin digital para presentarte a Empresas y Reclutadores.",
-        },
-        {
-         
-          title: "Creación de Perfiles RRSS",
-          description: "Te ayudamos a crear tus perfiles o mejorar los existentes en RRSS",
-        },
-        ];
 
   return (
     <Container className="how-my-courses-work">
