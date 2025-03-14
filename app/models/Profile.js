9f7dba0e71b45c869c2420cc97d58d7894842f3d
BLACKID25 <25ingchourio@gmail.com>
2025-03-14 11:53:20 -0300

    ACtualizado 14-03-24

--- app/models/Profile.js
+++ app/models/Profile.js
@@ -1,24 +1,27 @@
 import mongoose from 'mongoose';
 
 const ProfileSchema = mongoose.Schema({
-  name:           { type: String, required: true },
-  username:       { type: String, unique: true, required: true },
-  email:          { type: String, required: true, unique: true },
-  phone:          { type: String },
-  UrlLinkedin:    { type: String },
-  Urlgithub:      { type: String },
-  Urlinstagram:   { type: String },
-  Urlfacebook:    { type: String },
-  photo:          { type: String },
-  country:        { type: String },
-  Profesion:      { type: String },
-  ageExpe:        { type: String },
-  description:    { type: String },
-  curriCV:        { type: String },
+  name:           { type: String, required: true }, // NOMBRE Y APELLIDO 
+  username:       { type: String, unique: true, required: true }, // USERNAME CREADO AUTOMATICO PARA CREAR URL PERSONALIZADA
+  Profesion:      { type: String }, // PROFESION
+  ageExpe:        { type: String }, // AÑOS DE EXPERIENCIA
+  email:          { type: String, required: true, unique: true }, // CORREO ELECTRONICO
+  country:        { type: String }, // PAIS DE RESIDENCIA
+  phone:          { type: String }, // TELEFONO DE CONTACTO
+  UrlLinkedin:    { type: String }, // URL LINKEDIN
+  Urlgithub:      { type: String }, // URL GIT HUB
+  Urlinstagram:   { type: String }, // URL INSTAGRAM
+  Urlfacebook:    { type: String }, // URL FACEBOOK 
+  photo:          { type: String }, // FOTO
+  description:    { type: String }, // CAMPO PARA BREVE DESCRIPCION PROFESION Y EXPERIENCIAS
+  curriCV:        { type: String }, // CARGAR EL CURRICULO
   skills:         [String], // Lista de habilidades
-  createdAt:      { type: Date, default: Date.now },
+  nameproyect:         { type: String }, // BOMBRE DE PROYECTOS 
+  tecnologiproyect:    { type: String }, // TECNOLOGIAS DE PROYECTOS 
+  photoproyect:        { type: String }, // FOTOS DE PROYECTOS
+  descproyect:         { type: String }, // DESCRIPCION DE PROYECTOS
+ 
 });
 export const PerfilModel = mongoose.models.Profile || mongoose.model('Profile', ProfileSchema);
-
 
 
